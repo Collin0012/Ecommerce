763e819872635f5bc0e6909a9fe6f4468de9edce
Collings Mwangobola <collings12@gmail.com>
2021-09-03 12:33:06 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/* ========###=======Show menu=======###======== */
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/*===========###=========Remove Menu mobile==========###========== */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    //active link
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    //remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
